5be3013b6632100dc85496dec402b99dff454f3c
Alex Hinds <alex.hinds1@outlook.com>
2022-09-21 13:40:22 +0100

    feat - Comparing longest (red) & shortest laps (green)

--- js/index.js
+++ js/index.js
@@ -1,53 +1,53 @@
 // Creating variables for each of the buttons
-const startBtn = document.querySelector(".start-btn");
-const resetBtn = document.querySelector(".reset-btn");
-const lapBtn = document.querySelector(".lap-btn");
-const stopBtn = document.querySelector(".stop-btn");
+const $startButton = document.querySelector(".start-btn");
+const $resetButton = document.querySelector(".reset-btn");
+const $lapButton = document.querySelector(".lap-btn");
+const $stopButton = document.querySelector(".stop-btn");
 const lapList = document.querySelector(".lap-time-list");
-const timeDisplay = document.querySelector(".time-display");
+const $timeDisplay = document.querySelector(".time-display");
 
 // save dom nodes with $
-
-let displaySeconds = document.querySelector(".seconds");
-let displayMinutes = document.querySelector(".minutes");
-let displayMilliSeconds = document.querySelector(".milliSeconds");
+// use object to have access to different grouped-variables
 
 let stopwatchInterval = 10;
-// use object to have access to different grouped-variables
 let milliseconds = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 let elaspedTime = 0;
-let currentDsiplayTime = 0;
-let startStopToggle = false;
-let startTimer;
-let shortestLap;
-let longestLap;
-let currentlap = 1;
-let lapArray = [];
+let currentDisplayTime = 0;
+let isStartTimer = false;
+let startTimerID;
+// **************************//
+let tableRow;
+let tableRowCurrentLap;
+let tableRowLapTime;
+let currentTableRow = 1;
 
-// let lapInterval;
-
-// creating timing function
 const startStopwatch = () => {
-  startStopToggle = true;
-  startTimer = setInterval(() => {
+  isStartTimer = true;
+  startTimerID = setInterval(() => {
     elaspedTime++;
-    currentDsiplayTime++;
-    console.log(elaspedTime + "ms");
-    timeDisplay.innerHTML = convertMillisecondsToTime(currentDsiplayTime);
+    currentDisplayTime++;
+    if (currentlap === 1) {
+      tableRow = table.insertRow(0);
+      tableRowCurrentLap = tableRow.insertCell(0);
+      tableRowLapTime = tableRow.insertCell(1);
+      tableRowCurrentLap.innerHTML = `Lap ${currentlap++}`;
+    }
+    $timeDisplay.innerHTML = getFormattedTime(currentDisplayTime);
+    tableRowLapTime.innerHTML = getFormattedTime(elaspedTime);
   }, stopwatchInterval);
 
   // conditional Buttons display
-  if (startStopToggle) {
-    startBtn.style.display = "none";
-    stopBtn.style.display = "block";
-    resetBtn.style.display = "none";
-    lapBtn.style.display = "block";
+  if (isStartTimer) {
+    $startButton.style.display = "none";
+    $stopButton.style.display = "block";
+    $resetButton.style.display = "none";
+    $lapButton.style.display = "block";
   }
 
-  lapBtn.style.backgroundColor = "var(--reset-btn-bg-color)";
+  $lapButton.style.backgroundColor = "var(--reset-btn-bg-color)";
 };
 
 //padding Functions
@@ -56,74 +56,108 @@ const padTo2Digits = (num) => {
 };
 
 // convertTime function
-const convertMillisecondsToTime = (milliseconds) => {
-  let milliSeconds = milliseconds % 100;
-  let seconds = Math.floor(milliseconds / 100);
-  let minutes = Math.floor(seconds / 60);
-  let hours = Math.floor(minutes / 60);
+const getFormattedTime = (duration) => {
+  milliseconds = duration % 100;
+  seconds = Math.floor(duration / 100);
+  minutes = Math.floor(seconds / 60);
+  hours = Math.floor(minutes / 60);
 
   seconds = seconds % 60;
   minutes = minutes % 60;
 
   if (minutes < 60) {
     return `${padTo2Digits(minutes)}:${padTo2Digits(seconds)}.${padTo2Digits(
-      milliSeconds
+      milliseconds
     )}`;
   } else {
     return `${padTo2Digits(hours)}:${padTo2Digits(minutes)}:${padTo2Digits(
       seconds
-    )}.${padTo2Digits(milliSeconds)}`;
+    )}.${padTo2Digits(milliseconds)}`;
   }
 };
 
 // Stopping function
 const stopTimer = () => {
-  clearInterval(startTimer);
-  startStopToggle = false;
+  clearInterval(startTimerID);
+  isStartTimer = false;
 
   // conditional Button display
-  if (!startStopToggle) {
-    startBtn.style.display = "block";
-    stopBtn.style.display = "none";
-    resetBtn.style.display = "block";
-    lapBtn.style.display = "none";
+  if (!isStartTimer) {
+    $startButton.style.display = "block";
+    $stopButton.style.display = "none";
+    $resetButton.style.display = "block";
+    $lapButton.style.display = "none";
   }
 };
 
 // Reset function
 const resetTimer = () => {
-  clearInterval(startTimer);
-  startStopToggle = false;
+  clearInterval(startTimerID);
+  isStartTimer = false;
   lapArray = [];
   currentlap = 1;
   seconds = 0;
   minutes = 0;
   milliseconds = 0;
-  displayMilliSeconds.innerHTML = "00";
-  displaySeconds.innerHTML = "00";
-  displayMinutes.innerHTML = "00";
-  lapList.innerHTML = "";
-  resetBtn.style.display = "none";
-  lapBtn.style.display = "block";
+  currentDisplayTime = 0;
+  elaspedTime = 0;
+  table.innerHTML = "";
+  $timeDisplay.innerHTML = getFormattedTime(currentDisplayTime);
+  $resetButton.style.display = "none";
+  $lapButton.style.display = "block";
 };
 
-// Lapping function
+let shortestLap;
+let longestLap;
+let currentlap = 1;
+let lapArray = [];
+let table = document.querySelector("table");
+
+const createRow = (num) => {
+  tableRow = table.insertRow(0);
+  tableRowCurrentLap = tableRow.insertCell(0);
+  tableRowLapTime = tableRow.insertCell(1);
+
+  tableRowCurrentLap.innerHTML = `Lap ${currentlap++}`;
+  tableRowLapTime.innerHTML = `${getFormattedTime(num)}`;
+  elaspedTime = 0;
+};
+
+const compareLap = () => {
+  longestLap = getFormattedTime(Math.max(...lapArray));
+  shortestLap = getFormattedTime(Math.min(...lapArray));
+
+  for (let i = 0, row; (row = table.rows[i + 1]); i++) {
+    for (let j = 0, col; (col = row.cells[j]); j++) {
+      if (currentlap >= 3 && shortestLap !== longestLap) {
+        if (col.innerText === longestLap) {
+          col.classList.add("longest-lap");
+        } else {
+          col.classList.remove("longest-lap");
+        }
+        if (col.innerText === shortestLap) {
+          col.classList.add("shortest-lap");
+        } else {
+          col.classList.remove("shortest-lap");
+        }
+      }
+    }
+  }
+};
+
 const lapSplitTime = () => {
-  if (elaspedTime === 0) {
+  if (elaspedTime === 0 && !isStartTimer) {
     return;
   } else {
     lapArray.push(elaspedTime);
-    console.log(convertMillisecondsToTime(elaspedTime));
-    lapList.innerHTML += `<li class="list-item"><p class="lap">Lap ${currentlap++}</p><p class="lap-time">${convertMillisecondsToTime(
-      elaspedTime
-    )}</p></li>`;
-    elaspedTime = 0;
+    createRow(elaspedTime);
+    compareLap();
   }
 };
 
 // .0NCLICK
 
-startBtn.addEventListener("click", startStopwatch);
-stopBtn.addEventListener("click", stopTimer);
-resetBtn.addEventListener("click", resetTimer);
-lapBtn.addEventListener("click", lapSplitTime);
+$startButton.addEventListener("click", startStopwatch);
+$stopButton.addEventListener("click", stopTimer);
+$resetButton.addEventListener("click", resetTimer);
+$lapButton.addEventListener("click", lapSplitTime);
