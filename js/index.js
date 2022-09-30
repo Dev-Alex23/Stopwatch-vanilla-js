cc49a00dd907c5a4c0471442df5a75f9e57d5e7f
Alex Hinds <alex.hinds1@outlook.com>
2022-09-30 17:42:50 +0100

    refactor - using objects to group variables and breaking down functions into smallers ones and importing them from utils file

--- js/index.js
+++ js/index.js
@@ -1,4 +1,9 @@
-import { getFormattedTime } from "./utils.js";
+import {
+  getFormattedTime,
+  stopwatchData,
+  compareLaps,
+  lapData,
+} from "./utils.js";
 
 const $startButton = document.querySelector(".start-btn");
 const $resetButton = document.querySelector(".reset-btn");
@@ -6,44 +11,35 @@ const $lapButton = document.querySelector(".lap-btn");
 const $stopButton = document.querySelector(".stop-btn");
 const $timeDisplay = document.querySelector(".time-display");
 
-let currentLapElapsedTime = 0;
-let currentDisplayTime = 0;
-let isStartTimer = false;
-let startTimerID;
-let currentLap = 1;
-let lapArray = [];
-
 const updateTimer = () => {
-  currentLapElapsedTime++;
-  currentDisplayTime++;
-  if (currentLap === 1) {
-    createRow(currentLapElapsedTime);
+  stopwatchData.currentLapElapsedTime++;
+  stopwatchData.currentDisplayTime++;
+  if (stopwatchData.currentLap === 1) {
+    createRow();
   }
-  startTimerID = setTimeout(updateTimer, 10);
-  $timeDisplay.innerText = getFormattedTime(currentDisplayTime);
-  tableRowLapTime.innerText = getFormattedTime(currentLapElapsedTime);
+  stopwatchData.startTimerID = requestAnimationFrame(updateTimer);
+  $timeDisplay.innerText = getFormattedTime(stopwatchData.currentDisplayTime);
+  lapData.tableRowLapTime.innerText = getFormattedTime(
+    stopwatchData.currentLapElapsedTime
+  );
 };
 
 const startStopwatch = () => {
-  isStartTimer = true;
-  startTimerID = setTimeout(updateTimer, 10);
-
-  // conditional Buttons display
-  if (isStartTimer) {
+  stopwatchData.isStartTimer = true;
+  requestAnimationFrame(updateTimer);
+  if (stopwatchData.isStartTimer) {
     $startButton.style.display = "none";
     $stopButton.style.display = "block";
     $resetButton.style.display = "none";
     $lapButton.style.display = "block";
   }
-
-  $lapButton.style.backgroundColor = "var(--reset-btn-bg-color)";
 };
 
 const stopTimer = () => {
-  clearInterval(startTimerID);
-  isStartTimer = false;
+  cancelAnimationFrame(stopwatchData.startTimerID);
+  stopwatchData.isStartTimer = false;
 
-  if (!isStartTimer) {
+  if (!stopwatchData.isStartTimer) {
     $startButton.style.display = "block";
     $stopButton.style.display = "none";
     $resetButton.style.display = "block";
@@ -52,75 +48,43 @@ const stopTimer = () => {
 };
 
 const resetTimer = () => {
-  clearInterval(startTimerID);
-  isStartTimer = false;
-  lapArray = [];
-  currentLap = 1;
-  shortestLap = Infinity;
-  longestLap = -1;
-  currentDisplayTime = 0;
-  currentLapElapsedTime = 0;
+  cancelAnimationFrame(stopwatchData.startTimerID);
+  stopwatchData.currentLapElapsedTime = 0;
+  stopwatchData.currentDisplayTime = 0;
+  stopwatchData.isStartTimer = false;
+  stopwatchData.startTimerID = 0;
+  stopwatchData.currentLap = 1;
+  stopwatchData.lapArray = [];
   $table.innerText = "";
   $resetButton.style.display = "none";
   $lapButton.style.display = "block";
-  $timeDisplay.innerText = getFormattedTime(currentDisplayTime);
+  $timeDisplay.innerText = getFormattedTime(stopwatchData.currentDisplayTime);
 };
 
 const $table = document.querySelector("table");
-const getAllTableRow = document.getElementsByTagName("table")[0].rows;
 
-let shortestLap = Infinity;
-let longestLap = -1;
-let tableRow;
-let tableRowCurrentLap;
-let tableRowLapTime;
-
-export const createRow = () => {
-  tableRow = $table.insertRow(0);
-  tableRowCurrentLap = tableRow.insertCell(0);
-  tableRowLapTime = tableRow.insertCell(1);
-  tableRowLapTime.innerText = currentLapElapsedTime;
-  tableRowCurrentLap.innerText = `Lap ${currentLap++}`;
-
-  if (lapArray.length === 2) {
-    if (currentLapElapsedTime > lapArray[0]) {
-      longestLap = lapArray[1];
-      shortestLap = lapArray[0];
-      getAllTableRow[1].classList.add("longest-lap");
-      getAllTableRow[2].classList.add("shortest-lap");
-    } else {
-      longestLap = lapArray[0];
-      shortestLap = lapArray[1];
-      getAllTableRow[2].classList.add("longest-lap");
-      getAllTableRow[1].classList.add("shortest-lap");
-    }
-  }
-
-  if (lapArray.length >= 3) {
-    if (longestLap < currentLapElapsedTime) {
-      longestLap = currentLapElapsedTime;
-      getAllTableRow[1].classList.add("longest-lap");
-      console.log("Longest Lap", longestLap);
-      let removeOldLongestLap = document.getElementsByClassName("longest-lap");
-      removeOldLongestLap[1].classList.remove("longest-lap");
-    }
-    if (currentLapElapsedTime < shortestLap) {
-      shortestLap = currentLapElapsedTime;
-      getAllTableRow[1].classList.add("shortest-lap");
-      let removeOldShortestLap =
-        document.getElementsByClassName("shortest-lap");
-      removeOldShortestLap[1].classList.remove("shortest-lap");
-    }
-  }
-  currentLapElapsedTime = 0;
+const createRow = () => {
+  lapData.tableRow = $table.insertRow(0);
+  lapData.tableRowCurrentLap = lapData.tableRow.insertCell(
+    0
+  ).innerText = `Lap ${stopwatchData.currentLap++}`;
+  lapData.tableRowLapTime = lapData.tableRow.insertCell(1);
+  compareLaps();
+  stopwatchData.currentLapElapsedTime = 0;
 };
 
 const lapSplitTime = () => {
-  if (currentLapElapsedTime === 0 && !isStartTimer) {
+  if (
+    stopwatchData.currentLapElapsedTime === 0 ||
+    !stopwatchData.isStartTimer
+  ) {
     return;
   } else {
-    lapArray = [...lapArray, currentLapElapsedTime];
-    createRow(currentLapElapsedTime);
+    stopwatchData.lapArray = [
+      ...stopwatchData.lapArray,
+      stopwatchData.currentLapElapsedTime,
+    ];
+    createRow();
   }
 };
 
--- js/utils.js
+++ js/utils.js
@@ -27,3 +27,57 @@ export const getFormattedTime = (duration) => {
     )}.${padTo2Digits(milliseconds)}`;
   }
 };
+
+export const stopwatchData = {
+  currentLapElapsedTime: 0,
+  currentDisplayTime: 0,
+  isStartTimer: false,
+  startTimerID: 0,
+  currentLap: 1,
+  lapArray: [],
+};
+export const lapData = {
+  shortestLap: Infinity,
+  longestLap: -1,
+  tableRow: null,
+  tableRowLapTime: null,
+  tableRowCurrentLap: null,
+};
+
+const $getAllTableRow = document.getElementsByTagName("table")[0].rows;
+
+const checkFirstLap = () => {
+  if (stopwatchData.lapArray.length === 2) {
+    if (stopwatchData.currentLapElapsedTime > stopwatchData.lapArray[0]) {
+      lapData.longestLap = stopwatchData.lapArray[1];
+      lapData.shortestLap = stopwatchData.lapArray[0];
+      $getAllTableRow[1].classList.add("longest-lap");
+      $getAllTableRow[2].classList.add("shortest-lap");
+    } else {
+      lapData.longestLap = stopwatchData.lapArray[0];
+      lapData.shortestLap = stopwatchData.lapArray[1];
+      $getAllTableRow[2].classList.add("longest-lap");
+      $getAllTableRow[1].classList.add("shortest-lap");
+    }
+  }
+};
+
+export const compareLaps = () => {
+  checkFirstLap();
+
+  if (stopwatchData.lapArray.length >= 3) {
+    if (lapData.longestLap < stopwatchData.currentLapElapsedTime) {
+      lapData.longestLap = stopwatchData.currentLapElapsedTime;
+      $getAllTableRow[1].classList.add("longest-lap");
+      let removeOldLongestLap = document.getElementsByClassName("longest-lap");
+      removeOldLongestLap[1].classList.remove("longest-lap");
+    }
+    if (stopwatchData.currentLapElapsedTime < lapData.shortestLap) {
+      lapData.shortestLap = stopwatchData.currentLapElapsedTime;
+      $getAllTableRow[1].classList.add("shortest-lap");
+      let removeOldShortestLap =
+        document.getElementsByClassName("shortest-lap");
+      removeOldShortestLap[1].classList.remove("shortest-lap");
+    }
+  }
+};
